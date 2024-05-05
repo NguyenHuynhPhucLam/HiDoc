8e3d1e92fefd8bae92a65d4c335861e6e6bd8a5b
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-05 15:59:31 +0700

    Create New User from web

---
+++ src/containers/System/UserManage.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUsers } from '../../services/userService';
+import ModalUser from './ModalUser';
+
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+      isOpenModalUser: false,
+    };
+  }
+  async componentDidMount() {
+    let response = await getAllUsers('ALL');
+    if (response && response.errCode === 0) {
+      this.setState({
+        arrUsers: response.users,
+      });
+    }
+    console.log('Get users from nodejs: ', response);
+  }
+
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
+
+  render() {
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className='users-container'>
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          test={'abc'}
+          toggleFromParent={this.toggleUserModal}
+        />
+        <div className='title text-center'>Manage users with React</div>
+        <div className='mx-1'>
+          <button
+            className='btn btn-primary px-3'
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i class='fas fa-plus'></i> Add a new user
+          </button>
+        </div>
+        <div className='users-table mt-3 mx-1'>
+          <table id='customers'>
+            <tr>
+              <th>Emaik</th>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>Address</th>
+              <th>Action</th>
+            </tr>
+            {arrUsers &&
+              arrUsers.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{item.email}</td>
+                    <td>{item.firstName}</td>
+                    <td>{item.lastName}</td>
+                    <td>{item.address}</td>
+                    <td>
+                      <button className='btn-edit'>
+                        <i class='fas fa-pencil-alt'></i>
+                      </button>
+                      <button className='btn-delete'>
+                        <i class='fas fa-trash'></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
