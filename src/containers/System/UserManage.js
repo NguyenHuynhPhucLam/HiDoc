a003951e94d7cc15f2f9fa2a5d5c7b427f3feb32
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-06 23:34:15 +0700

    Create New User from React

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss';
-import { getAllUsers } from '../../services/userService';
+import { getAllUsers, createNewUserService } from '../../services/userService';
 import ModalUser from './ModalUser';
 
 class UserManage extends Component {
@@ -14,6 +14,10 @@ class UserManage extends Component {
     };
   }
   async componentDidMount() {
+    await this.getAllUsersFromReact();
+  }
+
+  getAllUsersFromReact = async () => {
     let response = await getAllUsers('ALL');
     if (response && response.errCode === 0) {
       this.setState({
@@ -21,7 +25,7 @@ class UserManage extends Component {
       });
     }
     console.log('Get users from nodejs: ', response);
-  }
+  };
 
   handleAddNewUser = () => {
     this.setState({
@@ -35,14 +39,30 @@ class UserManage extends Component {
     });
   };
 
+  createNewUser = async (data) => {
+    try {
+      let response = await createNewUserService(data);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        await this.getAllUsersFromReact();
+        this.setState({
+          isOpenModalUser: false,
+        });
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   render() {
     let arrUsers = this.state.arrUsers;
     return (
       <div className='users-container'>
         <ModalUser
           isOpen={this.state.isOpenModalUser}
-          test={'abc'}
           toggleFromParent={this.toggleUserModal}
+          createNewUser={this.createNewUser}
         />
         <div className='title text-center'>Manage users with React</div>
         <div className='mx-1'>
@@ -50,37 +70,40 @@ class UserManage extends Component {
             className='btn btn-primary px-3'
             onClick={() => this.handleAddNewUser()}
           >
-            <i class='fas fa-plus'></i> Add a new user
+            <i className='fas fa-plus'></i> Add a new user
           </button>
         </div>
         <div className='users-table mt-3 mx-1'>
           <table id='customers'>
-            <tr>
-              <th>Emaik</th>
-              <th>Firstname</th>
-              <th>Lastname</th>
-              <th>Address</th>
-              <th>Action</th>
-            </tr>
-            {arrUsers &&
-              arrUsers.map((item, index) => {
-                return (
-                  <tr key={index}>
-                    <td>{item.email}</td>
-                    <td>{item.firstName}</td>
-                    <td>{item.lastName}</td>
-                    <td>{item.address}</td>
-                    <td>
-                      <button className='btn-edit'>
-                        <i class='fas fa-pencil-alt'></i>
-                      </button>
-                      <button className='btn-delete'>
-                        <i class='fas fa-trash'></i>
-                      </button>
-                    </td>
-                  </tr>
-                );
-              })}
+            <tbody>
+              <tr>
+                <th>Emaik</th>
+                <th>Firstname</th>
+                <th>Lastname</th>
+                <th>Address</th>
+                <th>Action</th>
+              </tr>
+
+              {arrUsers &&
+                arrUsers.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.email}</td>
+                      <td>{item.firstName}</td>
+                      <td>{item.lastName}</td>
+                      <td>{item.address}</td>
+                      <td>
+                        <button className='btn-edit'>
+                          <i className='fas fa-pencil-alt'></i>
+                        </button>
+                        <button className='btn-delete'>
+                          <i className='fas fa-trash'></i>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
           </table>
         </div>
       </div>
