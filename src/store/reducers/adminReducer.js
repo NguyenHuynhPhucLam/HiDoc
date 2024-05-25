05d2ea89a5bfba1dc7705df5b663da2be84bd488
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-25 16:30:52 +0700

    Done 58

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -1,30 +1,57 @@
 import actionTypes from '../actions/actionTypes';
 
 const initialState = {
+  isLoadingGender: false,
   genders: [],
   roles: [],
-  position: [],
+  positions: [],
 };
 
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
+    // GENDER
     case actionTypes.FETCH_GENDER_START:
+      state.isLoadingGender = true;
       return {
         ...state,
       };
     case actionTypes.FETCH_GENDER_SUCCESS:
-      let copyState = { ...state };
-      copyState.genders = action.data;
-      return {
-        ...copyState,
-      };
-    case actionTypes.FETCH_GENDER_FAILED:
+      state.isLoadingGender = false;
+      state.genders = action.data;
       return {
         ...state,
       };
-
+    case actionTypes.FETCH_GENDER_FAILED:
+      state.isLoadingGender = false;
+      state.genders = [];
+      return {
+        ...state,
+      };
+    // POSITION
+    case actionTypes.FETCH_POSITION_SUCCESS:
+      state.positions = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_POSITION_FAILED:
+      state.positions = [];
+      return {
+        ...state,
+      };
+    // ROLE
+    case actionTypes.FETCH_ROLE_SUCCESS:
+      state.roles = action.data;
+      return {
+        ...state,
+      };
+    case actionTypes.FETCH_ROLE_FAILED:
+      state.roles = [];
+      return {
+        ...state,
+      };
     default:
       return state;
   }
 };
+
 export default adminReducer;
