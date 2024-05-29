51050da8eab657b09223ee3b42b69aa31eaf0879
NguyenHuynhPhucLam <21520316@gm.uit.edu.vn>
2024-05-29 23:48:46 +0700

    Create ProfileDoctor Component

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -4,6 +4,7 @@ import { FormattedMessage } from 'react-intl';
 import './BookingModal.scss';
 import { Modal } from 'reactstrap';
 import ProfileDoctor from '../ProfileDoctor';
+import _ from 'lodash';
 class BookingModal extends Component {
   constructor(props) {
     super(props);
@@ -17,6 +18,10 @@ class BookingModal extends Component {
   render() {
     // toggle= {}
     let { isOpenModal, closeBookingModal, dataTime } = this.props;
+    let doctorId = '';
+    if (dataTime && !_.isEmpty(dataTime)) {
+      doctorId = dataTime.doctorId;
+    }
     return (
       <Modal
         isOpen={isOpenModal}
@@ -32,9 +37,9 @@ class BookingModal extends Component {
             </span>
           </div>
           <div className='booking-modal-body'>
-            {/* {JSON.stringify(dataTime)} */}
-            <div className='doctor-info'></div>
-            <div className='price'>Giá khám 500.000VND</div>
+            <div className='doctor-info'>
+              <ProfileDoctor doctorId={doctorId} />
+            </div>
             <div className='row'>
               <div className='col-6 form-group'>
                 <label>Họ Tên</label>
--- src/containers/Patient/Doctor/ProfileDoctor.js
+++ src/containers/Patient/Doctor/ProfileDoctor.js
@@ -3,7 +3,8 @@ import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import './ProfileDoctor.scss';
 import { getProfileDoctorByIdService } from '../../../services/userService';
-
+import { LANGUAGES } from '../../../utils';
+import NumberFormat from 'react-number-format';
 class ProfileDoctor extends Component {
   constructor(props) {
     super(props);
@@ -13,16 +14,91 @@ class ProfileDoctor extends Component {
   }
 
   async componentDidMount() {
-    let id = this.props.doctorId;
+    let data = await this.getInfoDoctor(this.props.doctorId);
+    this.setState({
+      dataProfile: data,
+    });
+  }
+
+  getInfoDoctor = async (id) => {
+    let result = {};
     if (id) {
-      let res = getProfileDoctorByIdService(id);
+      let res = await getProfileDoctorByIdService(id);
+      if (res && res.errCode === 0) {
+        result = res.data;
+      }
+    }
+    return result;
+  };
+
+  async componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.language !== this.props.language) {
+    }
+    if (prevProps.doctorId !== this.props.doctorId) {
+      //   this.getInfoDoctor(this.props.doctorId);
     }
   }
 
-  async componentDidUpdate(prevProps, prevState, snapshot) {}
-
   render() {
-    return <div></div>;
+    let { dataProfile } = this.state;
+    let { language } = this.props;
+    let nameVi = '';
+    let nameEn = '';
+    if (dataProfile && dataProfile.positionData) {
+      nameVi = `${dataProfile.positionData.valueVi}, ${dataProfile.lastName} ${dataProfile.firstName}`;
+      nameEn = `${dataProfile.positionData.valueEn}, ${dataProfile.firstName} ${dataProfile.lastName}`;
+    }
+    return (
+      <div className='profile-doctor-container'>
+        <div className='intro-doctor'>
+          <div
+            className='content-left'
+            style={{
+              backgroundImage: `url(${
+                dataProfile && dataProfile.image ? dataProfile.image : ''
+              })`,
+            }}
+          ></div>
+          <div className='content-right'>
+            <div className='up'>
+              {language === LANGUAGES.VI ? nameVi : nameEn}
+            </div>
+            <div className='down'>
+              {dataProfile &&
+                dataProfile.Markdown &&
+                dataProfile.Markdown.description && (
+                  <span>{dataProfile.Markdown.description}</span>
+                )}
+            </div>
+          </div>
+        </div>
+        <div className='price'>
+          Giá khám:{' '}
+          {dataProfile &&
+            dataProfile.Doctor_Info &&
+            language === LANGUAGES.VI && (
+              <NumberFormat
+                className='currency'
+                value={dataProfile.Doctor_Info.priceTypeData.valueVi}
+                displayType={'text'}
+                thousandSeparator={true}
+                suffix={'VNĐ'}
+              />
+            )}
+          {dataProfile &&
+            dataProfile.Doctor_Info &&
+            language === LANGUAGES.EN && (
+              <NumberFormat
+                className='currency'
+                value={dataProfile.Doctor_Info.priceTypeData.valueEn}
+                displayType={'text'}
+                thousandSeparator={true}
+                suffix={'USD'}
+              />
+            )}
+        </div>
+      </div>
+    );
   }
 }
 
