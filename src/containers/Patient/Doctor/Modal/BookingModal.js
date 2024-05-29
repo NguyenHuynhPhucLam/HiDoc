6958bde21cca45aaaaa922d983cb1de48396a9e0
NguyenHuynhPhucThinh <phuclam1005@gmail.com>
2024-05-29 21:45:06 +0700

    New Modal Making

---
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import './BookingModal.scss';
+import { Modal } from 'reactstrap';
+
+class BookingModal extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    async componentDidMount() { }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+
+    }
+
+    render() {
+        return (
+            <Modal
+                isOpen={true}
+                className={'booking-modal-container'}
+                centered
+                size='lg'
+            >
+                <div className='booking-modal-content'>
+                    <div className='booking-modal-header'>
+                        <span className='left'>Thông tin đặt lịch khám bệnh</span>
+                        <span className='right'><i className='fas fa-times'></i></span>
+
+                    </div>
+                    <div className='booking-modal-body'>
+
+                    </div>
+                    <div className='booking-modal-footer'>
+                        <button className='btn-booking-confirm'>Xác nhận</button>
+                        <button className='btn-booking-cancel'>Cancel</button>
+                    </div>
+                </div>
+
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookingModal);
